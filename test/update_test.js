db21800eae1791852189014e246eda602dac3709
Sooraj Dube <soorajdube.is@gmail.com>
2018-07-28 11:56:28 +0530

    validatins

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('updating users', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({ name: 'joe' });
+        joe = new User({ name: 'joe', postCount: 0 });
         joe.save()
             .then(() => done())
     })
@@ -40,6 +40,15 @@ describe('updating users', () => {
 
     it('class update by id', (done) => {
         assertName(User.findByIdAndUpdate(joe._id,{name: 'Alex'}), done);      
+    })
+
+    it('increment post count by one', (done) => {
+        User.update({ name: 'joe'}, { $inc: {postCount: 1}})
+            .then(() => User.findOne({ name: 'joe' }))
+            .then((user) => {
+                assert(user.postCount === 1)
+                done();
+            })
     })
 
 })
